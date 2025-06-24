892b366a5b8b39ff6cb366c6df1f89fd7fb75ab0
kishorecheryala09 <kishore.cheryala@avanade.com>
2025-06-24 22:19:05 +0100

    Adding Infrastructure

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.3.7"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.43.0"
+    }
+  }
+  cloud { 
+    
+    organization = "ProjectWorkspaces" 
+
+    workspaces { 
+      name = "TerraformCI" 
+    } 
+  } 
+}
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = "East US"
+  name     = "rgdp"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "dpstorageaccount234"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
